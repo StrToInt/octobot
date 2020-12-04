0c15fa8c4a73a459aadd06437e895dbfb2e38861
strtoint <strtoint@mail.ru>
2020-12-04 12:11:00 +1000

    добавил инфы при печати

--- octobot.py
+++ octobot.py
@@ -5,6 +5,7 @@ import typing
 import configparser
 import requests
 import json
+import math
 from dataclasses import dataclass
 
 from aiogram import Bot, Dispatcher, executor, types
@@ -60,19 +61,35 @@ def get_printer_connection_status():
 
 #get printer state when connected
 def get_printer_state():
-    status = Printer_State()
+    state = Printer_State()
     try:
-        r = requests.get(url = octoprint+'/api/connection', headers = {'X-Api-Key':key})
+        r = requests.get(url = octoprint+'/api/printer', headers = {'X-Api-Key':key})
         if r.status_code == 200:
-            status.data = json.loads(r.text)
-            status.success = True
+            state.data = json.loads(r.text)
+            state.success = True
         else:
-            status.errorCode = str(r.status_code)
-            status.success = False
+            state.errorCode = str(r.status_code)
+            state.success = False
     except Exception:
-        status.success = False
+        state.success = False
     finally:
-        return status
+        return state
+
+#get job state when printing
+def get_printer_job_state():
+    job_state = Printer_State()
+    try:
+        r = requests.get(url = octoprint+'/api/job', headers = {'X-Api-Key':key})
+        if r.status_code == 200:
+            job_state.data = json.loads(r.text)
+            job_state.success = True
+        else:
+            job_state.errorCode = str(r.status_code)
+            job_state.success = False
+    except Exception:
+        job_state.success = False
+    finally:
+        return job_state
 
 
 
@@ -109,11 +126,38 @@ async def echo(message: types.Message):
 async def callback_status_command(query: types.CallbackQuery, callback_data: typing.Dict[str, str]):
     if check_user(query.message.chat.id):
         await query.answer("получение статуса...")  # don't forget to answer callback query as soon as possible\
-        status = get_printer_connection_status()
-        if status.success:
-            await bot.send_message(query.message.chat.id, status.state)
-        elif status.errorCode != '-1':
-            await bot.send_message(query.message.chat.id, 'Ошибка получения статуса!\n Код ответа: '+status.errorCode)
+        connection_status = get_printer_connection_status()
+        msg = ''
+        if connection_status.success:
+            if connection_status.state == 'Closed':
+                msg += 'Принтер выключен'
+            else:
+                msg += 'Принтер включен\n'
+                printer_state = get_printer_state()
+                if printer_state.success:
+                    if ( (printer_state.data['state']['flags']['printing'] == True) or
+                    (printer_state.data['state']['flags']['pausing'] == True) or
+                    (printer_state.data['state']['flags']['paused'] == True) or
+                    (printer_state.data['state']['flags']['resuming'] == True) ):
+                        #get job state if printing
+                        job_state = get_printer_job_state()
+                        msg += 'Принтер печатает\n'
+                        if job_state.success:
+                            msg += 'Файл: '+job_state.data['job']['file']['name']
+                            msg += '\nПримерное время печати: '+str(round(job_state.data['job']['estimatedPrintTime'],2))
+                            msg += '\nИзрасходуется: '+str(round(job_state.data['job']['filament']['tool0']['length'],2))+' мм / '+str(round(job_state.data['job']['filament']['tool0']['volume'],2))+' см³'
+                            msg += '\nПрогресс: '+str(round(job_state.data['progress']['completion'],2))+' %'
+                            msg += '\nВремя печати: '+str(job_state.data['progress']['printTime'])+' с'
+                            msg += '\nОсталось: '+str(job_state.data['progress']['printTimeLeft'])+' с'
+                        else:
+                            msg += 'Ошибка получения данных о печати'
+
+                    #msg += json.dumps(printer_state.data, indent=2)
+                else:
+                    msg += 'Ошибка получения данных о статусе'
+            await bot.send_message(query.message.chat.id, msg)
+        elif connection_status.errorCode != '-1':
+            await bot.send_message(query.message.chat.id, 'Ошибка получения статуса!\n Код ответа: '+connection_status.errorCode)
         else:
             await bot.send_message(query.message.chat.id, 'Подключение к OCTOPRINT не удалось')
 
