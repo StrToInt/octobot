ceaac738f1ca8e184f3fee1003f304fe172a322f
strtoint <strtoint@mail.ru>
2020-12-13 15:59:56 +1000

    типа предрелиз чтоль...

---
+++ start.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd /home/pi/octobot/
+screen -L -dmS octobot python3 /home/pi/octobot/octobot.py
+
---
+++ stop.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd /home/pi/octobot/
+screen -S octobot -X at 0 stuff ^C
